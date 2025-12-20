8d210fc918821cc145641bd3f52935a0607eac69
Prajjwal Shukla <5prajjwal14112000@gmail.com>
2025-12-20 18:15:12 -0500

    Fix | Add expirytime for JWT token

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -16,7 +16,7 @@ apiClient.interceptors.request.use((config => {
 }));
 
 apiClient.interceptors.response.use(response => response, error => {
-  if (error.response && error.response.status === 401) {
+  if (error.response && error.response.status === 401 && error.response.status === 403) {
     removeToken();
     window.location.href = '/login';
   }
